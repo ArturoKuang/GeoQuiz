5ef37d001da2d709a4647ee0839625a38eadec33
axk4647 <arturokuang@gmail.com>
2020-08-24 17:44:24 -0400

    added icons to prev and next button

--- java/com/example/geoguesser/MainActivity.kt
+++ java/com/example/geoguesser/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,7 +13,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton : Button
     private lateinit var falseButton : Button
-    private lateinit var nextButton : Button
+    private lateinit var nextButton : ImageButton
+    private lateinit var prevButton : ImageButton
     private lateinit var questionView : TextView
 
     private val questionBank = listOf(
@@ -32,6 +34,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
         questionView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
@@ -44,6 +47,13 @@ class MainActivity : AppCompatActivity() {
 
         nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
+            update()
+        }
+        
+        prevButton.setOnClickListener {
+            currentIndex = (currentIndex - 1)
+            if(currentIndex < 0)
+                currentIndex = questionBank.size - 1
             update()
         }
 
